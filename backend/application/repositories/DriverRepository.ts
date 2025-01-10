f841b1bdd9c9b2285e7646273c568eedfedf2b2f
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-01-10 21:31:47 +0100

    Remove unneeded methods from Driver repository

--- backend/application/repositories/DriverRepository.ts
+++ backend/application/repositories/DriverRepository.ts
@@ -1,10 +1,8 @@
 import { Driver } from "../../domain/entities/Driver";
 
 export interface DriverRepository {
-    save(driver: Driver): Promise<void>;
-    findByIdentifier(identifier: number): Promise<Driver | null>;
-    findByName(name: string): Promise<Driver | null>;
-    findAll(): Promise<Driver[]>;
-    delete(driver: Driver): Promise<void>;
-    exists(driver: Driver): Promise<boolean>;
+  save(driver: Driver): Promise<void>;
+  findByIdentifier(identifier: number): Promise<Driver | null>;
+  findAll(): Promise<Driver[]>;
+  delete(driver: Driver): Promise<void>;
 }
