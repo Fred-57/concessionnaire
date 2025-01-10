c7874754427ef7c173b83a90dd5a29225b02681a
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-01-10 21:49:06 +0100

    Add createdAt and updatedAt properties for Driver

--- backend/domain/entities/Driver.ts
+++ backend/domain/entities/Driver.ts
@@ -2,24 +2,30 @@ import { License } from "../types/License";
 import { Entity } from "./Entity";
 
 export class Driver implements Entity {
-    private constructor(
-        public readonly identifier: number,
-        public readonly name: string, 
-        public readonly license: License,
-        public readonly numberOfYearsOfExperience: number,
-    ) {}
+  private constructor(
+    public readonly identifier: number,
+    public readonly name: string,
+    public readonly license: License,
+    public readonly numberOfYearsOfExperience: number,
+    public readonly createdAt: Date,
+    public readonly updatedAt: Date,
+  ) {}
 
-    public static from(
-        identifier: number,
-        name: string, 
-        license: License,
-        numberOfYearsOfExperience: number,
-    ): Driver {
-        return new Driver(
-            identifier,
-            name,
-            license,
-            numberOfYearsOfExperience,
-        );
-    }
+  public static from(
+    identifier: number,
+    name: string,
+    license: License,
+    numberOfYearsOfExperience: number,
+    createdAt: Date,
+    updatedAt: Date,
+  ): Driver {
+    return new Driver(
+      identifier,
+      name,
+      license,
+      numberOfYearsOfExperience,
+      createdAt,
+      updatedAt,
+    );
+  }
 }
