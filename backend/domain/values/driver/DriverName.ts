4fd584ead2a71db695a7cf6e3df2df78601f290c
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-01-10 21:48:48 +0100

    Move DriverName value object to driver subdirectory

---
+++ backend/domain/values/driver/DriverName.ts
@@ -0,0 +1,17 @@
+import { DriverNameTooShortError } from "../../errors/driver/DriverNameTooShortError";
+import { Value } from "../Value";
+
+export class DriverName implements Value<string> {
+  private constructor(public readonly value: string) {}
+
+  public static from(name: string): DriverName {
+    const normalizedValue = name.trim().toLowerCase();
+
+    if (normalizedValue.length < 3) {
+      throw new DriverNameTooShortError();
+    }
+
+    return new DriverName(normalizedValue);
+  }
+}
+
