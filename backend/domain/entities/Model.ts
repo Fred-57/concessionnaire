0e8c532680b7ba799b09fd704cefe61039309ef6
Frederic <frederic.lopez9@hotmail.com>
2025-01-11 18:53:41 +0100

    Create Model domain

--- backend/domain/entities/Model.ts
+++ backend/domain/entities/Model.ts
@@ -1,24 +1,37 @@
+import { ModelName } from "@domain/values/model/ModelName";
 import { Entity } from "./Entity";
 
 export class Model implements Entity {
-    private constructor(
-        public readonly identifier: number,        
-        public readonly name: string,
-        public readonly repairMileage: number,
-        public readonly repairDeadline: Date,
-    ) {}
+  private constructor(
+    public readonly identifier: string,
+    public readonly name: ModelName,
+    public readonly repairMileage: number,
+    public readonly repairDeadline: Date,
+    public readonly createdAt: Date,
+    public readonly updatedAt: Date
+  ) {}
 
-    public static from(
-        identifier: number,
-        name: string,
-        repairMileage: number,
-        repairDeadline: Date,
-    ): Model {
-        return new Model(
-            identifier,
-            name,
-            repairMileage,
-            repairDeadline,
-        );
+  public static from(
+    identifier: string,
+    nameValue: string,
+    repairMileage: number,
+    repairDeadline: Date,
+    createdAt: Date,
+    updatedAt: Date
+  ) {
+    const name = ModelName.from(nameValue);
+
+    if (name instanceof Error) {
+      return name;
     }
+
+    return new Model(
+      identifier,
+      name,
+      repairMileage,
+      repairDeadline,
+      createdAt,
+      updatedAt
+    );
+  }
 }
