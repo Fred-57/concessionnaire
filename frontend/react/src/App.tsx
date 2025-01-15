599e7a99597f7ee901673ed29e36e88061033e0d
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-01-15 17:04:13 +0100

    Add common layout

--- frontend/react/src/App.tsx
+++ frontend/react/src/App.tsx
@@ -1,7 +1,14 @@
-import { Button } from "@/components/ui/button";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./pages/Home";
 
 function App() {
-  return <Button>Hello, world!</Button>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
