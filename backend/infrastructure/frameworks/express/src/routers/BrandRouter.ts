01e6867ddae835ed77cc7e6c59c6aa1754adcf74
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-01-20 21:04:02 +0100

    Add endpoints to read, update and delete a single brand

--- backend/infrastructure/frameworks/express/src/routers/BrandRouter.ts
+++ backend/infrastructure/frameworks/express/src/routers/BrandRouter.ts
@@ -1,5 +1,8 @@
-import { ListBrandsUsecase } from "@application/usecases/brand/ListBrandsUsecase";
 import { CreateBrandUsecase } from "@application/usecases/brand/CreateBrandUsecase";
+import { DeleteBrandUsecase } from "@application/usecases/brand/DeleteBrandUsecase";
+import { GetBrandUsecase } from "@application/usecases/brand/GetBrandUsecase";
+import { ListBrandsUsecase } from "@application/usecases/brand/ListBrandsUsecase";
+import { UpdateBrandUsecase } from "@application/usecases/brand/UpdateBrandUsecase";
 import { Brand } from "@domain/entities/Brand";
 import { BrandNameAlreadyTakenError } from "@domain/errors/brand/BrandNameAlreadyTakenError";
 import { BrandNameTooShortError } from "@domain/errors/brand/BrandNameTooShortError";
@@ -14,6 +17,27 @@ BrandRouter.get("/", async (_, res) => {
     new PostgresBrandRepository(),
   ).execute();
   res.status(StatusCodes.OK).json(brands);
+});
+
+BrandRouter.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const brands = await new ListBrandsUsecase(
+    new PostgresBrandRepository(),
+  ).execute();
+
+  if (!brands) {
+    res.sendStatus(StatusCodes.NOT_FOUND);
+    return;
+  }
+
+  const brand = brands.find((brand) => brand.identifier === id);
+
+  if (!brand) {
+    res.sendStatus(StatusCodes.NOT_FOUND);
+    return;
+  }
+
+  res.status(StatusCodes.OK).json(brand);
 });
 
 BrandRouter.post("/", async (req, res) => {
@@ -36,4 +60,60 @@ BrandRouter.post("/", async (req, res) => {
   }
 
   res.sendStatus(StatusCodes.CREATED);
+});
+
+BrandRouter.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const brand = await new GetBrandUsecase(
+    new PostgresBrandRepository(),
+  ).execute(id);
+
+  if (!brand) {
+    res.sendStatus(StatusCodes.NOT_FOUND);
+    return;
+  }
+
+  const updatedBrand = Brand.from(
+    brand.identifier,
+    name,
+    brand.createdAt,
+    brand.updatedAt,
+  );
+
+  if (updatedBrand instanceof BrandNameTooShortError) {
+    res.sendStatus(StatusCodes.UNPROCESSABLE_ENTITY);
+    return;
+  }
+
+  try {
+    await new UpdateBrandUsecase(new PostgresBrandRepository()).execute(
+      updatedBrand,
+    );
+  } catch (error) {
+    if (error instanceof BrandNameAlreadyTakenError) {
+      res.sendStatus(StatusCodes.CONFLICT);
+      return;
+    }
+  }
+
+  res.sendStatus(StatusCodes.OK);
+});
+
+BrandRouter.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const brand = await new GetBrandUsecase(
+    new PostgresBrandRepository(),
+  ).execute(id);
+
+  if (!brand) {
+    res.sendStatus(StatusCodes.NOT_FOUND);
+    return;
+  }
+
+  await new DeleteBrandUsecase(new PostgresBrandRepository()).execute(id);
+
+  res.sendStatus(StatusCodes.NO_CONTENT);
 });
