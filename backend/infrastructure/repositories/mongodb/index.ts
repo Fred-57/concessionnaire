6850ee651a09fc1b023a3a70c51651fe904ac642
Fred-57 <frederic.lopez9@hotmail.com>
2025-01-26 11:27:45 +0100

    Company: fix delete + check MongoRepository ok

--- backend/infrastructure/repositories/mongodb/index.ts
+++ backend/infrastructure/repositories/mongodb/index.ts
@@ -1,3 +1,4 @@
 export { connect } from "./src/db";
 export { MongoDriverRepository } from "./src/MongoDriverRepository";
 export { MongoModelRepository } from "./src/MongoModelRepository";
+export { MongoCompanyRepository } from "./src/MongoCompanyRepository";
