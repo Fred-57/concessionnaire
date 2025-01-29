aa4f8171bd57c19b6a6f5a82d4c5291d7ab051e8
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-01-29 22:36:41 +0100

    Implement frontend for PartOrderHistory

--- frontend/react/src/pages/Part.tsx
+++ frontend/react/src/pages/Part.tsx
@@ -1,7 +1,12 @@
 import { Layout } from "@/components/Layout";
+import { Button } from "@/components/ui/button";
 import { DataTable } from "@/components/ui/data-table";
 import { useToast } from "@/hooks/use-toast";
 import { PartType, columns } from "@/types/part";
+import {
+  PartOrderHistoryType,
+  columns as partOrderHistoryColumns,
+} from "@/types/partOrderHistory";
 import ky from "ky";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
@@ -11,6 +16,9 @@ export function Part() {
   const { toast } = useToast();
 
   const [parts, setParts] = useState<PartType[]>([]);
+  const [partOrderHistory, setPartOrderHistory] = useState<
+    PartOrderHistoryType[]
+  >([]);
 
   useEffect(() => {
     const fetchParts = async () => {
@@ -20,6 +28,32 @@ export function Part() {
 
     fetchParts();
   }, []);
+
+  useEffect(() => {
+    const fetchPartOrderHistory = async () => {
+      let partOrderHistoryApi = (await ky
+        .get("/express/partOrderHistory")
+        .json()) as PartOrderHistoryType[];
+
+      partOrderHistoryApi = partOrderHistoryApi.map((partOrderHistory) => {
+        const part = parts.find(
+          (p) => p.identifier === partOrderHistory.partIdentifier.value
+        ) as PartType;
+        return {
+          ...partOrderHistory,
+          partIdentifier: {
+            value: part.name.value,
+          },
+        };
+      });
+
+      setPartOrderHistory(partOrderHistoryApi as PartOrderHistoryType[]);
+    };
+
+    if (parts.length > 0) {
+      fetchPartOrderHistory();
+    }
+  }, [parts]);
 
   const goToUpdate = (part: PartType) => {
     navigate(`/parts/${part.identifier}`);
@@ -41,6 +75,36 @@ export function Part() {
     }
   };
 
+  const handleCancelOrder = async (order: PartOrderHistoryType) => {
+    const response = await ky.patch(
+      `/express/partOrderHistory/${order.identifier}/status`,
+      {
+        json: {
+          status: "CANCELED",
+        },
+      }
+    );
+
+    if (response.ok) {
+      toast({
+        title: "Commande annulée",
+      });
+
+      const newPartOrderHistory = partOrderHistory.map((p) => {
+        if (p.identifier === order.identifier) {
+          return { ...p, status: "CANCELED" };
+        }
+        return p;
+      });
+
+      setPartOrderHistory(newPartOrderHistory);
+    } else {
+      toast({
+        title: "Erreur lors de l'annulation",
+      });
+    }
+  };
+
   return (
     <Layout
       title="Pièces"
@@ -50,6 +114,51 @@ export function Part() {
       }}
     >
       <DataTable columns={columns({ goToUpdate, handleDelete })} data={parts} />
+
+      {parts.length > 0 && (
+        <PartOrderHistory
+          partOrderHistory={partOrderHistory}
+          handleCancelOrder={handleCancelOrder}
+        />
+      )}
     </Layout>
+  );
+}
+
+function PartOrderHistory({
+  partOrderHistory,
+  handleCancelOrder,
+}: {
+  partOrderHistory: PartOrderHistoryType[];
+  handleCancelOrder: (order: PartOrderHistoryType) => Promise<void>;
+}) {
+  const navigate = useNavigate();
+
+  const isAdmin =
+    localStorage.getItem("role") === "gestionnaire" ||
+    localStorage.getItem("is_admin") === "true"
+      ? true
+      : false;
+
+  return (
+    <section className="flex flex-col gap-5 mt-5">
+      <div className="flex justify-between items-center h-10">
+        <h1 className="text-2xl font-bold tracking-tight">
+          Historique des commandes
+        </h1>
+        {isAdmin && (
+          <Button
+            onClick={() => navigate("/partOrderHistory/create")}
+            className="self-start"
+          >
+            Commander
+          </Button>
+        )}
+      </div>
+      <DataTable
+        columns={partOrderHistoryColumns({ handleCancelOrder })}
+        data={partOrderHistory}
+      />
+    </section>
   );
 }
