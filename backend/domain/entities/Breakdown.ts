66686a88148b5a46a4f90ce192a40f71c0175221
Fred-57 <frederic.lopez9@hotmail.com>
2025-01-30 19:49:42 +0100

    Complete Domain for BreakdownEntity, without Parts for the moment

--- backend/domain/entities/Breakdown.ts
+++ backend/domain/entities/Breakdown.ts
@@ -1,12 +1,20 @@
 import { randomUUID } from "crypto";
 import { Entity } from "./Entity";
+import { StatusMaintenanceBreakdownEnum } from "@domain/types/StatusMaintenanceBreakdownEnum";
+import { BreakdownDate } from "@domain/values/breakdown/BreakdownDate";
+import { DateBehindNowError } from "@domain/errors/DateBehindNowError";
+import { BreakdownTotalCost } from "@domain/values/breakdown/BreakdownTotalCost";
+import { BreakdownTotalCostLessThanZeroError } from "@domain/errors/breakdown/BreakdownTotalCostLessThanZero";
 
 export class Breakdown implements Entity {
   private constructor(
     public readonly identifier: string,
-    public readonly date: Date,
+    public readonly date: BreakdownDate,
     public readonly description: string,
     public readonly rentalIdentifier: string,
+    public readonly status: StatusMaintenanceBreakdownEnum,
+    // TODO Parts
+    public readonly totalCost: BreakdownTotalCost,
     public readonly createdAt: Date,
     public readonly updatedAt: Date
   ) {}
@@ -16,14 +24,28 @@ export class Breakdown implements Entity {
     date: Date,
     description: string,
     rentalIdentifier: string,
+    status: string,
+    totalCost: number,
     createdAt: Date,
     updatedAt: Date
   ) {
+    const breakdownDate = BreakdownDate.from(date);
+    if (breakdownDate instanceof DateBehindNowError) {
+      return breakdownDate;
+    }
+
+    const breakdownTotalCost = BreakdownTotalCost.from(totalCost);
+    if (breakdownTotalCost instanceof BreakdownTotalCostLessThanZeroError) {
+      return breakdownTotalCost;
+    }
+
     return new Breakdown(
       identifier,
-      date,
+      breakdownDate,
       description,
       rentalIdentifier,
+      status as StatusMaintenanceBreakdownEnum,
+      breakdownTotalCost,
       createdAt,
       updatedAt
     );
@@ -32,7 +54,9 @@ export class Breakdown implements Entity {
   public static create(
     date: Date,
     description: string,
-    rentalIdentifier: string
+    rentalIdentifier: string,
+    status: string,
+    totalCost: number
   ) {
     const identifier = randomUUID();
     const createdAt = new Date();
@@ -43,6 +67,8 @@ export class Breakdown implements Entity {
       date,
       description,
       rentalIdentifier,
+      status,
+      totalCost,
       createdAt,
       updatedAt
     );
